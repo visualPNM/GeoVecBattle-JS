fe1226ec6c3ece0a6fabb3c45cde2a87fd39a86b
퍄녀미 (VisualPNM) <breaksreak@gmail.com>
2017-05-12 19:52:46 -0400

    Update GeoVecBattle.js

--- GeoVecBattle.js
+++ GeoVecBattle.js
@@ -1,4 +1,9 @@
-function prime(x) {
+function abs(x) { //절댓값 구하기
+  if(x >= 0) { return x; }
+  else { return -x; }
+}
+
+function prime(x) { // 333 이하의 소수 판정하기
   var a;
   var prime = 1;
   for(a = 2; (a < 18 && a < x); a++)
@@ -12,3 +17,4 @@ function prime(x) {
   if(x<2){ prime = 0; }
   return prime;
 }
+
