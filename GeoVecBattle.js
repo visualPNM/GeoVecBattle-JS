7b4e3c1fb17abbe4cc23b7a1ed1d4e804875be45
퍄녀미 (VisualPNM) <breaksreak@gmail.com>
2017-05-12 19:38:56 -0400

    Update GeoVecBattle.js

--- GeoVecBattle.js
+++ GeoVecBattle.js
@@ -1,1 +1,14 @@
-nothing here yet
+function prime(x) {
+  var a;
+  var prime = 1;
+  for(a = 2; (a < 18 && a < x); a++)
+  {
+    if(x % a == 0)
+    {
+      prime = 0;
+      break;
+    }
+  }
+  if(x<2){ prime = 0; }
+  return prime;
+}
