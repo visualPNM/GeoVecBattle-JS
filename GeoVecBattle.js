895bef43e3521a8038f95064fc3152097d583d02
퍄녀미 (VisualPNM) <breaksreak@gmail.com>
2017-05-12 23:02:19 -0400

    Update GeoVecBattle.js

--- GeoVecBattle.js
+++ GeoVecBattle.js
@@ -29,3 +29,35 @@ var random;
 var speed;
 var real_speed;
 var sosu;
+var nameA;
+var nameB;
+
+nameA = '김철수'; //수정가능
+nameB = '이영희'; //수정가능
+speed = 5; //수정가능
+
+length[0] = nameA.length;
+length[1] = nameB.length;
+
+//speed
+if(speed == 0) { real_speed = 2000; }
+else if(speed == 1) { real_speed = 1500; }
+else if(speed == 2) { real_speed = 1000; }
+else if(speed == 3) { real_speed = 750; }
+else if(speed == 4) { real_speed = 600; }
+else if(speed == 5) { real_speed = 450; }
+else if(speed == 6) { real_speed = 333; }
+else if(speed == 7) { real_speed = 250; }
+else if(speed == 8) { real_speed = 167; }
+else if(speed == 9) { real_speed = 100; }
+else { real_speed = 750; }
+
+// turn
+if(nameA > nameB) { turn = 1; }
+else if(nameA < nameB) { turn = 0; }
+
+for(i = 0; unitA[i] == unitB[i]; i++)
+{
+  if(unitA[i+1] > unitB[i+1]) { turn = 1; }
+  else if(unitA[i+1] < unitB[i+1]) { turn = 0; }
+}
