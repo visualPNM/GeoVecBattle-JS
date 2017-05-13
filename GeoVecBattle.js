182cb619456fa9acb5f6287f6e01e2b0d33f61cc
퍄녀미 (VisualPNM) <breaksreak@gmail.com>
2017-05-13 16:07:17 -0400

    Update GeoVecBattle.js

--- GeoVecBattle.js
+++ GeoVecBattle.js
@@ -18,8 +18,8 @@ function prime(x) { // 333 이하의 소수 판정하기
   return prime;
 }
 
-var specA = new Array(5); // spec : 0(hit point), 1(att), 2(def), 3(critical/normal), 4(probability of critical)
-var specB = new Array(5);
+var specA = [0, 0, 0, 0, 0]; // spec : 0(hit point), 1(att), 2(def), 3(critical/normal), 4(probability of critical)
+var specB = [0, 0, 0, 0, 0];
 var length = new Array(2); // length of a, b
 var unitA = [];
 var unitB = [];
@@ -81,3 +81,40 @@ for(i = 0; unitA[i] == unitB[i]; i++)
   else if(unitA[i+1] < unitB[i+1]) { turn = 0; }
 }
 
+for(var i = 0; i < nameA.length; i++) // character 1
+  {
+    if(unitA[i] !== ' ') {
+      specA[0] += abs(unitA[i]);
+      specA[1] += 2*abs(unitA[i]);
+      specA[2] *= abs(unitA[i]);
+      specA[2] += abs(unitA[i]) + 7;
+      specA[3] *= abs(unitA[i]);
+      specA[3] += 2*abs(unitA[i]) + 7;
+      specA[4] += abs(unitA[i])*abs(unitA[i]);
+        
+      specA[0] = 100 + specA[0] % 234;
+      specA[1] = 10 + specA[1] % 24;
+      specA[2] = specA[2] % 333;
+      specA[3] = specA[1] * (specA[3] % 3 + 3);
+      specA[4] = 3 + specA[4] % 31;
+    }
+}
+
+for(var i = 0; i < nameB.length; i++) // character 1
+  {
+    if(unitB[i] !== ' ') {
+      specB[0] += abs(unitB[i]);
+      specB[1] += 2*abs(unitB[i]);
+      specB[2] *= abs(unitB[i]);
+      specB[2] += abs(unitB[i]) + 7;
+      specB[3] *= abs(unitB[i]);
+      specB[3] += 2*abs(unitB[i]) + 7;
+      specB[4] += abs(unitB[i])*abs(unitB[i]);
+        
+      specB[0] = 100 + specB[0] % 234;
+      specB[1] = 10 + specB[1] % 24;
+      specB[2] = specB[2] % 333;
+      specB[3] = specB[1] * (specB[3] % 3 + 3);
+      specB[4] = 3 + specB[4] % 31;
+    }
+}
